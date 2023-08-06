a97cbb1aa7949ddb9850dc780cc170285a9a14b6
Björn Grüning <bjoern.gruening@gmail.com>
2023-08-06 14:37:51 +1000

    cleanup some comments

--- startapp.sh
+++ startapp.sh
@@ -1,20 +1,6 @@
 #!/bin/sh -f
 
-# script to run Ocean Data View 64 bit on a Linux 64 bit system
-
-# edit the lines immediately following #TODO, set execute attribute and
-# move to a directory in your path. USAGE: run_odv [collection]
-
-# © 2023 Reiner Schlitzer,
-#        Alfred Wegener Institute,
-#        Bremerhaven, Germany
-#        Reiner.Schlitzer@awi.de
-
-
-
-#-----------------------------------------------------------------------
-# nothing to be changed below this line
+# The path `/opt/qupath/infile.tiff` is important as the Galaxy IT is inserting the user-chosen file into this path.
 
 HOME="/tmp"
-
 _JAVA_OPTIONS="-Duser.home=${HOME} -Djavafx.cachedir=/tmp" exec /opt/qupath/QuPath/bin/QuPath --image /opt/qupath/infile.tiff --quiet
